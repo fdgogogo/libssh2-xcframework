64b88880de1fc64a3cf6539f5ab8d30a945e7e84
Jiaan Fang <jiaanf@gmail.com>
2023-12-29 10:19:30 +0800

    add package products

--- Package.swift
+++ Package.swift
@@ -9,7 +9,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "libssh2-xcframework",
-            targets: ["libssh2-xcframework", "libssl-xcframework", "libcrypto-xcframework"]),
+            targets: ["libssh2-xcframework"]),
+        .library(
+            name: "libssl-xcframework",
+            targets: ["libssl-xcframework"]),
+        .library(
+            name: "libcrypto-xcframework",
+            targets: ["libcrypto-xcframework"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
